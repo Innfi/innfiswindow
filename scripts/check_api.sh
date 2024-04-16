2ff219d76d7598d55c4e149eb991b694aa922909
innfi <innfi.world@gmail.com>
2024-04-16 22:38:45 +0900

    ready to fetch data and express

--- scripts/check_api.sh
+++ scripts/check_api.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-curl --cacert ~/.minikube/ca.crt --cert ~/.minikube/profiles/minikube/client.crt --key ~/.minikube/profiles/minikube/client.key $1
+ENDPOINT=$(kubectl cluster-info | grep 'Kubernetes control plane' | awk '/http/ {print $NF}')
+curl --cacert ~/.minikube/ca.crt --cert ~/.minikube/profiles/minikube/client.crt --key ~/.minikube/profiles/minikube/client.key ${ENDPOINT}
