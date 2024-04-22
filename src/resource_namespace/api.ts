a4f92a9f31e0128d3c1a98dad78f149ae3743ffe
innfi <innfi.world@gmail.com>
2024-04-22 23:21:25 +0900

    building pages for namespace

--- src/resource_namespace/api.ts
+++ src/resource_namespace/api.ts
@@ -1,13 +1,24 @@
 import axios from 'axios';
 import { useQuery } from 'react-query';
+import fs from 'fs';
 
-export const useGetNamespaceList = () => {
+export const useGetNamespaceUnit = <T>() => {
   const url = process.env.REACT_APP_BACKEND_URL ? `${process.env.REACT_APP_BACKEND_URL}/namespace` : '/namespace';
 
-  return useQuery('getNamespaceList', async () => {
-    axios.get(url).then((res) => {
-      return res.data as NamespaceUnit;
+  const cacert = fs.readFileSync('~/.minikube/ca.crt', 'utf8');
+  const clientcrt = fs.readFileSync('~/.minikube/client.crt', 'utf8');
+  const clientKey = fs.readFileSync('~/.minikube/client.key', 'utf8');
+
+  const service = async () => {
+    return await axios.get<T>(url, {
+      httpsAgent: {
+        ca: cacert,
+        cert: clientcrt,
+        key: clientKey,
+      },
     });
-  });
+  };
+
+  return useQuery('getNamespaceList', service);
 };
 
--- src/resource_namespace/list.tsx
+++ src/resource_namespace/list.tsx
@@ -1,13 +1,36 @@
-import { useGetNamespaceList } from "./api";
+import { useEffect, useState } from "react";
+import { useGetNamespaceUnit } from "./api";
+import { Grid } from "@mui/material";
+
+const placeholder: NamespaceUnit = {
+  kind: "",
+  apiVersion: "",
+  metadata: {
+    resourceVersion: "",
+  },
+  items: [],
+};
 
 export function NamespaceListPage() {
-  const { data, isFetched } = useGetNamespaceList();
-
+  const { data, isFetched } = useGetNamespaceUnit<NamespaceUnit>();
   console.log(`data: ${JSON.stringify(data)}`);
 
+  const [namespaceUnit, setNamespaceUnit] = useState<NamespaceUnit>(placeholder);
+  useEffect(() => {
+    if (isFetched) {
+      setNamespaceUnit(data?.data || placeholder);
+    }
+  });
+
   return (
-    <div>
-      start from here
-    </div>
+    <Grid container spacing={3}>
+      {namespaceUnit.items.map((item) => {
+        return (
+          <Grid item xs={12} key={item.metadata.uid}>
+            {item.metadata.name}
+          </Grid>
+        );
+      })}
+    </Grid>
   );
 }
