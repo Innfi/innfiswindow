23427cf2f3c5d0132741d9143f310a8923d47a8f
innfi <innfi.world@gmail.com>
2024-04-26 21:43:59 +0900

    removing redundant scripts

--- scripts/call_api.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-ENDPOINT=$(kubectl cluster-info | grep 'Kubernetes control plane' | awk '/http/ {print $NF}')
-curl --cacert ~/.minikube/ca.crt --cert ~/.minikube/profiles/minikube/client.crt --key ~/.minikube/profiles/minikube/client.key ${ENDPOINT}$1
