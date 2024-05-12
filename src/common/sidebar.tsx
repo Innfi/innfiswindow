5cfce2da27502d169f781dd1667b989f9a505984
innfi <innfi.world@gmail.com>
2024-05-12 21:08:29 +0900

    tinkering layout

--- src/common/sidebar.tsx
+++ src/common/sidebar.tsx
@@ -24,47 +24,49 @@ export function Sidebar() {
   };
 
   return (
-    <Box sx={{ display: 'flex' }}>
-      <Drawer
-        sx={{
-          width: 240,
-          flexShrink: 0,
-          '&.MuiDrawer-paper': {
-            width: 240,
-            boxSizing: 'border-box',
-          },
-        }}
-        variant="permanent"
-        anchor="left"
-      >
-        <List>
-          <ListItemButton>
-            <ListItemText  />
-          </ListItemButton>
-        </List>
-        <List>
-          {items.map((item: DrawerItem) => (
-            <ListItem key={item.text} disablePadding onClick={() => handleClick(item)}>
-              <ListItemButton>
-                <ListItemIcon>
-                  <item.icon />
-                </ListItemIcon>
-                <ListItemText primary={item.text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
+    <Box sx={{ display: 'flex', flexDirection: 'column'}}>
       <Box sx={{ display: 'flex', flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" sx={{ flexGrow: 1 }}>toolbar test</Typography>
-        </Toolbar>
-      </AppBar>
+        <AppBar position="fixed">
+          <Toolbar sx={{ marginLeft: "240px" }}>
+            <Typography variant="h6" sx={{ flexGrow: 1 }}>toolbar test</Typography>
+          </Toolbar>
+        </AppBar>
       </Box>
-      <Box component="main" sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}>
-        <Outlet />
-      </Box>
+      <Box sx={{ display: 'flex', marginTop: "100px" }}>
+        <Drawer
+          sx={{
+            width: 240,
+            marginTop: "200px" ,
+            flexShrink: 0,
+            '&.MuiDrawer-paper': {
+              boxSizing: 'border-box',
+            },
+          }}
+          variant="permanent"
+          anchor="left"
+        >
+          <List>
+            <ListItemButton>
+              <ListItemText  />
+            </ListItemButton>
+          </List>
+          <List>
+            {items.map((item: DrawerItem) => (
+              <ListItem key={item.text} disablePadding onClick={() => handleClick(item)}>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <item.icon />
+                  </ListItemIcon>
+                  <ListItemText primary={item.text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
+        <Box component="main" sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3, alignItems: 'center' }}>
+          <Outlet />
+        </Box>
+      </Box>    
     </Box>
   );
 }
