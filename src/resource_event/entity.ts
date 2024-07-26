c63c933e36bc8d72aaa59d079c040a3ce091b93c
innfi <innfi.world@gmail.com>
2024-07-26 22:45:50 +0900

    events: how to get latest events only, and periodically?

---
+++ src/resource_event/entity.ts
@@ -0,0 +1,22 @@
+// EventList
+export interface EventList {
+  kind: string;
+  apiVersion: string;
+
+  metadata: {
+    continue: string;
+    remainingItemCount: number;
+  }
+
+  items: EventSummary[];
+}
+
+export interface EventSummary {
+  type: string;
+  note: string;
+  regarding: {
+    kind: string;
+    namespace: string;
+    name: string;
+  };
+}
