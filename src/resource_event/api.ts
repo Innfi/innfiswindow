e3ed50cf1f1f7a1bbd169df58186de81f6611256
innfi <innfi.world@gmail.com>
2024-07-31 23:10:56 +0900

    finding how to periodically refetch for events

--- src/resource_event/api.ts
+++ src/resource_event/api.ts
@@ -3,10 +3,10 @@ import { useQuery } from 'react-query';
 import { axiosInstance } from '../common/axios.client';
 import { EventList } from './entity';
 
-export const useGetAllEvents = (limit: number, continueFlag?: string) => {
+export const useGetAllEvents = (limit: number, continueKey?: string) => {
   let url = `/apis/events.k8s.io/v1/events?limit=${limit}`;
-  if (continueFlag && continueFlag.length > 0) {
-    url = `${url}&contiunue=${continueFlag}`;
+  if (continueKey && continueKey.length > 0) {
+    url = `${url}&contiunue=${continueKey}`;
   }
 
   const service = async () => {
--- src/resource_event/list.tsx
+++ src/resource_event/list.tsx
@@ -1,3 +1,50 @@
+import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import { Grid, Paper, Stack, Table, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+
+import { initialErrorMessage } from "../common/app.state";
+import { ApiError } from "../common/axios.client";
+import { useGetAllEvents } from "./api";
+import { EventSummary } from "./entity";
+
+const FETCH_EVENT_COUNT = 10;
+
 export function EventListPage() {
-  return <div>event list dummy page</div>;
+  const [, setErrMsg] = useRecoilState(initialErrorMessage);
+  const [continueKey, setContinueKey] = useState('');
+
+  const { data, isFetched, refetch } = useGetAllEvents(FETCH_EVENT_COUNT, continueKey);
+  const [events, setEvents] = useState<EventSummary[]>([]);
+
+  useEffect(() => {
+    if (data instanceof ApiError) {
+      setErrMsg(`EventListPage] ${data.errMsg}`);
+    }
+
+    if (data?.data?.items) {
+      setEvents(data?.data?.items ? data?.data?.items : []);
+    }
+  }, [isFetched, data, setErrMsg]);
+
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={12}>
+        <Stack>
+          <TableContainer component={Paper}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Type</TableCell>
+                  <TableCell>Kind</TableCell>
+                  <TableCell>Note</TableCell>
+                  <TableCell>Name</TableCell>
+                  <TableCell>Namespace</TableCell>
+                </TableRow>
+              </TableHead>
+            </Table>
+          </TableContainer>
+        </Stack>
+      </Grid>
+    </Grid>
+  );
 }
