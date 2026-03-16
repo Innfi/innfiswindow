2d180c0a30f89d3bd445bc59dba5514352f24c11
innfi <innfi.world@gmail.com>
2026-03-16 23:03:10 +0900

    feature: ralph

--- src/resource_event/menu.ts
+++
@@ -1,7 +0,0 @@
-import VolumeUpTwoToneIcon from '@mui/icons-material/VolumeUpTwoTone';
-
-import { DrawerItem } from '../pathfinder/drawer.item';
-
-export const eventMenu: DrawerItem[] = [
-  { text: 'Events', path: '/events', icon: VolumeUpTwoToneIcon },
-];
--- src/resource_pod/menu.ts
+++
@@ -1,5 +0,0 @@
-import SailingTwoToneIcon from '@mui/icons-material/SailingTwoTone';
-
-import { DrawerItem } from '../pathfinder/drawer.item';
-
-export const podMenu: DrawerItem[] = [{ text: 'Pods', path: '/pods', icon: SailingTwoToneIcon }];
