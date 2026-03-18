8f2e63a22fc90554fd162bb590b1f7341449d01f
innfi <innfi.world@gmail.com>
2026-03-18 23:15:57 +0900

    feat: S01 Project scaffold with Electron + React + TypeScript + Tailwind + shadcn

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,9 @@
+function App(): JSX.Element {
+  return (
+    <div className="flex h-screen flex-col bg-background text-foreground">
+      <h1 className="p-4 text-xl font-semibold">Innfiswindow</h1>
+    </div>
+  )
+}
+
+export default App
